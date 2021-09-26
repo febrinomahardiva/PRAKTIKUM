829a361c11e39325e18942dbec8aaac7702780ce
febrinomahardiva <febrinomahardiva@gmail.com>
2021-09-27 00:11:21 +0800

    Jurnal1.java

---
+++ Praktikum/Jurnal1.java
@@ -0,0 +1,8 @@
+public class Jurnal1 {
+
+	public static void main(String[] args) {
+		System.out.println("Nama  : Febrino Mahardiva Akbar");
+		System.out.println("NIM   : 6706213034");
+		System.out.println("Kelas : D3RPLA45-03");
+	}
+}
